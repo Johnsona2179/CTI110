c9e008e61ed0e605ce5ee33ba189c3bed42dbb2f
Johnsona2179 <160092197+Johnsona2179@users.noreply.github.com>
2024-02-14 23:06:55 -0500

    Add files via upload

---
+++ P1LAB1_JohnsonAijah.py
@@ -0,0 +1,12 @@
+# Aijah Johnson
+
+# February 14, 2024
+
+# P1LAB1
+
+# Write a program that takes a first name as the input, and outputs a welcome message to that name.
+
+
+user_name = input()
+print('Hey', user_name)
+print('Welcome to zyBooks!')
